c658f3d9395d5a2a8098cc7699a7ae1c8bca28fb
Ailton Duarte <adidas.coyas@gmail.com>
2025-06-24 18:10:02 +0100

    first commit

---
+++ src/main/java/cv/terrasystem/zebratravelb/ZebratravelBApplication.java
@@ -0,0 +1,13 @@
+package cv.terrasystem.zebratravelb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZebratravelBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZebratravelBApplication.class, args);
+    }
+
+}
